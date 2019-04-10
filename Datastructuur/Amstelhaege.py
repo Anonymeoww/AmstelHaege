8906b85ffc43c5d26e4eaf6ef1bdd47a9537b79b
jesscsam <github@jessicasam.nl>
2019-04-10 13:38:10 +0200

    refresh

--- Datastructuur/Amstelhaege.py
+++ Datastructuur/Amstelhaege.py
@@ -8,10 +8,20 @@ class Amstelhaege():
         """
         Create houses & water
         """
-        self.houses = self.load_houses(TXTFILE, numhouses)
-        self.water = self.load_water()
+        self.houses = self.load_houses(properties.txt)
+        # self.water = self.load_water()
 
-    def load_houses(self, TXTFILE, numhouses):
+    def load_houses(self, filename):
+
+        houses_data = []
+
+        with open(filename, "r") as f:
+            for line in f:
+                house_data = []
+                house_data.append(line)
+                houses_data.append(house_data)
+                print(house_data)
+        print(houses_data)
 
         # should return list of dictionaries for each house. Each dictionary is {'house_id': 'x,y'}
 
@@ -19,4 +29,8 @@ class Amstelhaege():
     def load_water(self):
 
         # should return list of dictionaries for each water body? Each dictionary is {'water_id': 'x,y'}
-        #woooooop
+
+
+if __name__ == "__main__":
+
+    Amstelhaege = 
