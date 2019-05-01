69d2c3dd5a44ccb7c9568b45ea98c09fbc62cb92
Anonymeoww <47354277+Anonymeoww@users.noreply.github.com>
2019-05-01 14:11:24 +0200

    DCJ 01-05-2019 adjusted code ready for implementation in other files

--- Dilisha.py
+++ Dilisha.py
@@ -3,22 +3,10 @@ import math
 
 def iter():
     """
-    create fake neighbor
+    can be used to create neighboring solution
     """
-
-    iter = random.uniform(0.0,2.0)
-    iterround = round(iter, 2)
-
-    return iterround
-
-def calc_worth(itervalue):
-    """
-    calculate fake worth
-    """
-
-    worth = itervalue*10
-
-    return worth
+    # Should return the value of the neighboring solution (wijk waarde)
+    pass
 
 def sim_an_chance(current_worth, next_worth, temperature):
     """
@@ -31,38 +19,47 @@ def sim_an_chance(current_worth, next_worth, temperature):
 
 def calc_temp(i, current_worth):
     """
-    calculate temperature for current solution
+    calculate temperature for current solution, scheme must be updated for viable solutions
     """
-    if i = 1:
-        temperature = 0.2 * current_worth
+    if i == 1:
+        temperature = 0.8 * current_worth
     else:
         temperature = 0.5 * current_worth
 
     return temperature
 
-if __name__ == "__main__":
+def sim_an(current_worth):
 
-    current_worth = 5000
+    current_worth = current_worth
+    best_worth = current_worth
+    #for loop needs stop condition
     for i in range(1, 10):
-        best_worth = current_worth
-        itervalue = iter()
-        next_worth = calc_worth(itervalue)
+        next_worth = iter()
+        print(f'current: {current_worth}')
+        print(f'next: {next_worth}')
 
-        #als de buur beter is, dan buur accepteren
+        #als de buur beter is dan huidig, dan buur accepteren
         if next_worth > current_worth:
             current_worth = next_worth
 
             if next_worth > best_worth:
                 best_worth = next_worth
+                print(f'best: {best_worth}')
 
         #als de buur slechter is, bereken temperatuur en kans
         elif next_worth < current_worth:
             temperature = calc_temp(i, current_worth)
             chance = sim_an_chance(current_worth, next_worth, temperature)
+            print(f'chance: {chance}')
 
             #als de kans groter is dan de radomwaarde, accepteer de buur
             randomvalue = random.uniform(0.0, 1.0)
-            if (chance > randomvalue) or (chance = randomvalue):
+            if (chance > randomvalue) or (chance == randomvalue):
                 current_worth = next_worth
         else:
             pass
+    print(f'best: {best_worth}')
+
+if __name__ == "__main__":
+    current_worth = 5 #adjust value to relevant
+    sim_an(current_worth)
