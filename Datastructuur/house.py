9aee956f481e2c68b2141e8bcce9eab2846893a1
Anonymeoww <47354277+Anonymeoww@users.noreply.github.com>
2019-05-17 10:53:09 +0200

    DCJ 17-05-2019 changed name to type

--- Datastructuur/house.py
+++ Datastructuur/house.py
@@ -3,12 +3,12 @@ class House(object):
     Representation of a house in AmstelHaege
     """
 
-    def __init__(self, id, name, depth, height, minvr, extra_wpm, worth):
+    def __init__(self, id, type, depth, height, minvr, extra_wpm, worth):
         """
         Initializes a House
         """
         self.id = id
-        self.name = name
+        self.type = type
         self.depth = depth
         self.height = height
         self.minvr = minvr
@@ -16,4 +16,4 @@ class House(object):
         self.worth = worth
 
     def __str__(self):
-        return f"{self.name}"
+        return f"{self.type}"
