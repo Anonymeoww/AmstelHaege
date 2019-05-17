9569e8ef8578a47f43891950f38ca1442260ede9
JAkkerman <akkermanjoos@gmail.com>
2019-05-17 11:27:38 +0200

    JA 17-05-2019 (4)

--- wijk.py
+++ wijk.py
@@ -5,6 +5,8 @@ import visualize as vis
 import math
 
 import start_greedy_perhouse
+import start
+import start_greedy_fullwijk
 import helpers
 
 """
@@ -12,21 +14,23 @@ Hier komt een tekst te staan
 """
 
 HOUSES_NUMBER = 10
-BREADTH = 18
-HEIGHT = 16
+BREADTH = 320
+HEIGHT = 360
 ITERATIONS = 1000
+startmethods = [start, start_greedy_fullwijk, start_greedy_perhouse]
+startmethod = startmethods[0]
 
 if __name__ == "__main__":
 
     # initieer wijk, plot , visualiseer en geef waarde aan huizen en wijk
-    houses_list = start_greedy_perhouse.init(HOUSES_NUMBER, BREADTH, HEIGHT)
+    houses_list = startmethod.init(HOUSES_NUMBER, BREADTH, HEIGHT)
     #print(houses_list)
     houses_list = helpers.omlig_ruimte(houses_list, BREADTH, HEIGHT)
     #print(houses_list)
     houses_list, curr_waarde = helpers.waarde(houses_list)
     #print(f"Waarde wijk: {curr_waarde}")
     best_worth = curr_waarde
-    vis.grid(houses_list)
+    vis.grid(houses_list, best_worth)
 
     # # verander random positie huis annealing en plot
     # done_iterations = 0
