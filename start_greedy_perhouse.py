971f556f9c0e631707e4a86e3879b6110a84d07e
JAkkerman <akkermanjoos@gmail.com>
2019-05-20 12:37:26 +0200

    JA 20-05-2019 (3)

--- start_greedy_perhouse.py
+++ start_greedy_perhouse.py
@@ -8,9 +8,9 @@ def init(houses_number, BREADTH, HEIGHT):
     """
     Initialiseert een greedy wijk, plaatst PER HUIS de beste optie
     """
-    eensgezins = {'type':'eensgezinswoning', 'width': 16, 'depth': 16, 'worth': 285000, 'minvr': 4}
-    bungalow = {'type': 'bungalow', 'width': 20, 'depth': 15, 'worth': 399000, 'minvr': 6}
-    maison = {'type': 'maison', 'width': 22, 'depth': 21, 'worth': 610000, 'minvr': 12}
+    eensgezins = {'type':'eensgezinswoning', 'width': 16, 'depth': 16, 'base_worth': 285000, 'minvr': 4, 'extra': 3}
+    bungalow = {'type': 'bungalow', 'width': 20, 'depth': 15, 'base_worth': 399000, 'minvr': 6, 'extra': 4}
+    maison = {'type': 'maison', 'width': 22, 'depth': 21, 'base_worth': 610000, 'minvr': 12, 'extra': 6}
 
     houses_list = []
     house_id = 1
@@ -24,13 +24,13 @@ def init(houses_number, BREADTH, HEIGHT):
     type = maison
     ymin, ymax, xmin, xmax = helpers.gen_rand_coord(BREADTH, HEIGHT, type['depth'], type['width'], type['minvr'])
 
-    house = House(house_id, type['type'], xmin, xmax, ymin, ymax, type['minvr'], None, type['width'], type['depth'], type['worth'])
+    house = House(house_id, type['type'], xmin, xmax, ymin, ymax, type['minvr'], 0, type['width'], type['depth'], type['extra'], type['base_worth'], type['base_worth'])
     houses_list.append(house)
 
     numb_maison = numb_maison + 1
 
     houses_list = helpers.omlig_ruimte(houses_list, BREADTH, HEIGHT)
-    houses_list, wijkwaarde = helpers.waarde(houses_list)
+    wijkwaarde = helpers.waarde(houses_list)
 
     temp_houses_list = houses_list
 
@@ -48,7 +48,7 @@ def init(houses_number, BREADTH, HEIGHT):
             type = eensgezins
             numb_eensgezins = numb_eensgezins - 1
 
-        temphouse = house = House(house_id, type['type'], xmin, xmax, ymin, ymax, type['minvr'], None, type['width'], type['depth'], type['worth'])
+        temphouse = House(house_id, type['type'], xmin, xmax, ymin, ymax, type['minvr'], 0, type['width'], type['depth'], type['extra'], type['base_worth'], type['base_worth'])
         temp_houses_list.append(temphouse)
 
         tries = 0
@@ -62,13 +62,13 @@ def init(houses_number, BREADTH, HEIGHT):
                 temphouse.ymax = ymax
                 temphouse.xmin = xmin
                 temphouse.xmax = xmax
-                temphouse.worth = temphouse.update_worth()
+                temphouse.update_worth()
 
                 if helpers.check_surr(house_id, temp_houses_list, BREADTH, HEIGHT):
                     # print("yeet")
                     placed = True
 
-            temp_houses_list, new_worth = helpers.waarde(temp_houses_list)
+            new_worth = helpers.waarde(temp_houses_list)
 
             if new_worth > wijkwaarde:
                 wijkwaarde = new_worth
