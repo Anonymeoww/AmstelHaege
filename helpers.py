57422742e28b87bb672d197f08731c31627d1efb
JAkkerman <akkermanjoos@gmail.com>
2019-05-24 20:28:50 +0200

    JA 24-05-2019 (4)

--- helpers.py
+++ helpers.py
@@ -106,21 +106,6 @@ def omlig_ruimte(water_list, houses_list, BREADTH, HEIGHT):
 
                     house = pythagoras(combinations, other_combinations, house_coords, house)
 
-                    # for combination in combinations:
-                    #     for other_combination in other_combinations:
-                    #         dist = sqrt((pow((other_combination[0]-combination[0]), 2) + pow((other_combination[1]-combination[1]), 2)))
-                    #
-                    #         # seeks the lowest distance
-                    #         if house.olr > dist:
-                    #             house.olr = dist
-                    #
-                    # # check if houses do not overlap
-                    # for other_combination in other_combinations:
-                    #     if other_combination[0] >= x1min + house.minvr and other_combination[0] <= x1max + house.minvr and \
-                    #     other_combination[1] >= y1min + house.minvr and other_combination[1] <= y1max + house.minvr:
-                    #         house.olr = 0
-                    #         break
-
             #check distance to border, if smaller than current olr, change
             if x1min < house.olr:
                 house.olr = x1min
@@ -131,9 +116,8 @@ def omlig_ruimte(water_list, houses_list, BREADTH, HEIGHT):
             elif HEIGHT - y1max < house.olr:
                 house.olr = HEIGHT - y1max
 
-            # print(house.olr)
-
     return houses_list
+
 
 def pythagoras(combinations, other_combinations, house_coords, house):
     """
@@ -159,45 +143,35 @@ def pythagoras(combinations, other_combinations, house_coords, house):
     return house
 
 def gen_water(BREADTH, HEIGHT):
+    """
+    Generates water in predetermined places
+    """
 
     houses_list = []
     water_list = []
     water_id = 1
-    water_depth = 137.5
-    water_width = 44
+    water_depth = 80.5
+    water_width = 71.6
 
-    # while len(water_list) < 4:
-    #
-    #     ymin, ymax, xmin, xmax = gen_rand_coord(BREADTH, HEIGHT, water_depth, water_width, 4)
-    #     water = Water(water_id, xmin, xmax, ymin, ymax, water_width, water_depth, 4)
-    #     water_list.append(water)
     water_combinations = [[0, HEIGHT], [0, water_depth], [BREADTH - water_width, HEIGHT], [BREADTH - water_width, water_depth]]
 
+    # generate the coordinates for water
     for water_combination in water_combinations:
         xmin = water_combination[0]
         ymax = water_combination[1]
         xmax = water_combination[0] + water_width
         ymin = water_combination[1] - water_depth
-        print(xmin, ymin)
         water = Water(water_id, xmin, xmax, ymin, ymax, water_width, water_depth, 4)
         water_list.append(water)
 
-        # while check_surr(water_id, houses_list, water_list, BREADTH, HEIGHT) == False:
-        #     # print("yeet")
-        #     ymin, ymax, xmin, xmax = gen_rand_coord(BREADTH, HEIGHT, water_depth, water_width, 4)
-        #     water.ymin = ymin
-        #     water.ymax = ymax
-        #     water.xmin = xmin
-        #     water.xmax = xmax
-
         water_id = water_id + 1
 
-
-
-    print(water_list)
     return water_list
 
 def gen_rand_coord(BREADTH, HEIGHT, depth, width, minvr):
+    """
+    Randomly generates coordinates in the vicinity of the house
+    """
 
     i = ran.randint(1, (BREADTH - minvr))
     j = ran.randint(1, (HEIGHT - depth - minvr))
@@ -210,6 +184,9 @@ def gen_rand_coord(BREADTH, HEIGHT, depth, width, minvr):
     return ymin, ymax, xmin, xmax
 
 def gen_quadr_coord(depth, width, minvr, temp_houses_list):
+    """
+
+    """
 
     xminQ, xmaxQ, yminQ, ymaxQ = check_lowest_dens(temp_houses_list)
 
@@ -225,7 +202,9 @@ def gen_quadr_coord(depth, width, minvr, temp_houses_list):
 
 
 def check_lowest_dens(temp_houses_list):
-    "Checks which quadrant has the lowest density houses"
+    """
+    Checks which quadrant has the lowest density in houses
+    """
 
     q1 = 0
     q2 = 0
@@ -253,4 +232,3 @@ def check_lowest_dens(temp_houses_list):
         return 180, 360, 0, 160
     else:
         return 0, 360, 0, 320
-
